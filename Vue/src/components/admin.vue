56c21eb21305dfefcf8405fd0077ac6730c23617
karlaparra <karla.parra@mail.udp.cl>
2023-06-26 21:18:19 -0400

    prueba

--- Vue/src/components/admin.vue
+++ Vue/src/components/admin.vue
@@ -6,7 +6,7 @@
     <title>Hola mundo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     </head>
-<body>
+<body> 
     <header>
     <div class="inner">
       <div class="logo"><img src="../assets/fundacion.png"></div>
@@ -48,3 +48,4 @@
 <style>
     @import '../assets/css/admincss.css';
 </style>
+<!--esto es un comentario!>
